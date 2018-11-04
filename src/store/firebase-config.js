82206ef286d6aaec35047bfc00af42fe44f0ffca
Roman Stankiewicz <roman.stan3@gmail.com>
2018-11-04 16:00:31 +0000

    completed

---
+++ src/store/firebase-config.js
@@ -0,0 +1,10 @@
+
+const config = {
+  apiKey: "",
+  authDomain: "roman-stan.firebaseapp.com",
+  databaseURL: "https://roman-stan.firebaseio.com",
+  projectId: "roman-stan",
+  storageBucket: "",
+  messagingSenderId: "153596773654"
+}
+export default config
